7bd1f734349cea722c2e833fdf0179417492ade5
BexterBailey <bexstein02@gmail.com>
2024-10-09 08:31:18 -0400

    Added Player Rotation

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 100f; // Speed of the player
+    public float rotateSpeed = 100f;
 
     void Update()
     {
@@ -15,5 +16,15 @@ public class PlayerMovement : MonoBehaviour
         Vector3 direction = new Vector3(horizontal, 0, vertical);
 
         transform.Translate(direction * speed * Time.deltaTime);
+
+	//Vector3 heading = new Vector3(moveHorizontal, 0.0f, moveVertical);
+	
+	//transform.rotation = Quaternion.LookRotation(heading);
+	//transform.Translate (movement * movementSpeed * Time.deltaTime, Space.World);
+
+	if (Input.GetKey(KeyCode.A))
+	transform.Rotate(-Vector3.up * rotateSpeed * Time.deltaTime);
+	else if (Input.GetKey(KeyCode.D))
+	transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
     }
 }
